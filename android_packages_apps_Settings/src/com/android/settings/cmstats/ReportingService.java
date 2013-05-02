215b270655f29337f5f6c1c17c8506f74dffbcbc
vet-note <nela@nela.moo>
2013-05-02 13:23:29 +0200

    first commit

---
+++ android_packages_apps_Settings/src/com/android/settings/cmstats/ReportingService.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cmstats;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
+
+import com.android.settings.R;
+
+public class ReportingService extends Service {
+    protected static final String TAG = "CMStats";
+
+    protected static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
+
+    protected static final String ANONYMOUS_ALARM_SET = "pref_anonymous_alarm_set";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "Sending anonymous statistics data..");
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                report();
+            }
+        };
+        thread.start();
+        return Service.START_REDELIVER_INTENT;
+    }
+
+    private void report() {
+        String deviceId = Utilities.getUniqueID(getApplicationContext());
+        String deviceName = Utilities.getDevice();
+        String deviceVersion = Utilities.getModVersion();
+        String deviceCountry = Utilities.getCountryCode(getApplicationContext());
+        String deviceCarrier = Utilities.getCarrier(getApplicationContext());
+        String deviceCarrierId = Utilities.getCarrierId(getApplicationContext());
+
+        Log.d(TAG, "SERVICE: Device ID=" + deviceId);
+        Log.d(TAG, "SERVICE: Device Name=" + deviceName);
+        Log.d(TAG, "SERVICE: Device Version=" + deviceVersion);
+        Log.d(TAG, "SERVICE: Country=" + deviceCountry);
+        Log.d(TAG, "SERVICE: Carrier=" + deviceCarrier);
+        Log.d(TAG, "SERVICE: Carrier ID=" + deviceCarrierId);
+
+        // report to google analytics
+        GoogleAnalytics ga = GoogleAnalytics.getInstance(this);
+        Tracker tracker = ga.getTracker(getString(R.string.ga_trackingId));
+        tracker.sendEvent(deviceName, deviceVersion, deviceCountry, null);
+        // this really should be set at build time...
+        // format of version should be:
+        // version[-date-type]-device
+        String[] parts = deviceVersion.split("-");
+        String deviceVersionNoDevice = null;
+        if (parts.length == 2) {
+            deviceVersionNoDevice = parts[0];
+        }
+        else if (parts.length == 4) {
+            deviceVersionNoDevice = parts[0] + "-" + parts[2];
+        }
+        if (deviceVersionNoDevice != null)
+            tracker.sendEvent("checkin", deviceName, deviceVersionNoDevice, null);
+        tracker.close();
+
+        // report to the cmstats service
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://stats.cyanogenmod.com/submit");
+        try {
+            List<NameValuePair> kv = new ArrayList<NameValuePair>(5);
+            kv.add(new BasicNameValuePair("device_hash", deviceId));
+            kv.add(new BasicNameValuePair("device_name", deviceName));
+            kv.add(new BasicNameValuePair("device_version", deviceVersion));
+            kv.add(new BasicNameValuePair("device_country", deviceCountry));
+            kv.add(new BasicNameValuePair("device_carrier", deviceCarrier));
+            kv.add(new BasicNameValuePair("device_carrier_id", deviceCarrierId));
+            httppost.setEntity(new UrlEncodedFormEntity(kv));
+            httpclient.execute(httppost);
+            getSharedPreferences("CMStats", 0).edit().putLong(ANONYMOUS_LAST_CHECKED,
+                    System.currentTimeMillis()).apply();
+        } catch (Exception e) {
+            Log.e(TAG, "Got Exception", e);
+        }
+        ReportingServiceManager.setAlarm(this);
+        stopSelf();
+    }
+}
---
+++ android_packages_apps_Settings/src/com/android/settings/vetnote/HybridSettings.java
@@ -0,0 +1,172 @@
+package com.android.settings.vetnote;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.util.ExtendedPropertiesUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HybridSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private PreferenceScreen mDpiScreen;
+    private ListPreference mUimode;
+    private Preference mNavbarHeight;
+    private CheckBoxPreference mAutoBackup;
+    private Preference mBackup;
+    private Preference mRestore;
+
+    private Context mContext;
+
+    private int mNavbarHeightProgress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+        Utils.setContext(mContext);
+
+        addPreferencesFromResource(R.xml.hybrid_settings);
+
+        mDpiScreen = (PreferenceScreen) findPreference("system_dpi");
+
+        mUimode = (ListPreference) findPreference("ui_mode");
+
+        int prop = ExtendedPropertiesUtils
+                .getActualProperty("com.android.systemui.layout");
+        mUimode.setValue(String.valueOf(prop));
+        mUimode.setSummary(mUimode.getEntry());
+        mUimode.setOnPreferenceChangeListener(this);
+
+        mNavbarHeight = findPreference("navbar_height");
+
+        mAutoBackup = (CheckBoxPreference) findPreference("dpi_groups_auto_backup");
+        mBackup = findPreference("dpi_groups_backup");
+        mRestore = findPreference("dpi_groups_restore");
+
+        boolean isAutoBackup = mContext.getSharedPreferences(Applications.PREFS_NAME, 0)
+                .getBoolean(Applications.PROPERTY_AUTO_BACKUP, false);
+
+        mAutoBackup.setChecked(isAutoBackup);
+
+        mRestore.setEnabled(Applications.backupExists());
+
+        updateSummaries();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        if (preference == mNavbarHeight) {
+            showNavbarHeightDialog();
+        } else if (preference == mBackup) {
+            Applications.backup(mContext);
+        } else if (preference == mRestore) {
+            Applications.restore(mContext);
+	    Utils.reboot();
+        } else if (preference == mAutoBackup) {
+            SharedPreferences settings = mContext.getSharedPreferences(
+                    Applications.PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean(Applications.PROPERTY_AUTO_BACKUP,
+                    ((CheckBoxPreference) preference).isChecked());
+            editor.commit();
+        }
+        updateSummaries();
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        if ("ui_mode".equals(key)) {
+            String layout = (String) objValue;
+            Applications.addSystemLayout(mContext, layout);
+        }
+
+        updateSummaries();
+        return true;
+    }
+
+    private void updateSummaries() {
+        int dpi = ExtendedPropertiesUtils
+                .getActualProperty("com.android.systemui.dpi");
+        mDpiScreen.setSummary(getResources().getString(
+                R.string.system_dpi_summary)
+                + " " + dpi);
+
+        int layout = ExtendedPropertiesUtils
+                .getActualProperty("com.android.systemui.layout");
+        int index = mUimode.findIndexOfValue(String.valueOf(layout));
+        mUimode.setSummary(mUimode.getEntries()[index]);
+
+        mRestore.setEnabled(Applications.backupExists());
+    }
+
+    private void showNavbarHeightDialog() {
+        Resources res = getResources();
+        String cancel = res.getString(R.string.cancel);
+        String ok = res.getString(R.string.ok);
+        String title = res.getString(R.string.navbar_height_title);
+        int savedProgress = Integer.parseInt(ExtendedPropertiesUtils 
+                .getProperty("com.android.systemui.navbar.dpi")) / 5; 
+
+        LayoutInflater factory = LayoutInflater.from(getActivity());
+        final View alphaDialog = factory.inflate(R.layout.seekbar_dialog, null);
+        SeekBar seekbar = (SeekBar) alphaDialog.findViewById(R.id.seek_bar);
+        final TextView seektext = (TextView) alphaDialog
+                .findViewById(R.id.seek_text);
+        OnSeekBarChangeListener seekBarChangeListener = new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekbar, int progress,
+                    boolean fromUser) {
+                mNavbarHeightProgress = seekbar.getProgress() * 5;
+                seektext.setText(mNavbarHeightProgress + "%");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekbar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekbar) {
+            }
+        };
+        seektext.setText((savedProgress * 5) + "%");
+        seekbar.setMax(20);
+        seekbar.setProgress(savedProgress);
+        seekbar.setOnSeekBarChangeListener(seekBarChangeListener);
+        new AlertDialog.Builder(getActivity())
+                .setTitle(title)
+                .setView(alphaDialog)
+                .setNegativeButton(cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                // nothing
+                            }
+                        })
+                .setPositiveButton(ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Applications.addProperty(mContext,
+                                "com.android.systemui.navbar.dpi",
+                                mNavbarHeightProgress, true);
+                    }
+                }).create().show();
+    }
+}
